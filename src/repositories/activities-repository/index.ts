3642e977f699df6f2f4fd77874ca22fcd3784aff
Tiago <tiagoalex0202@gmail.com>
2023-08-14 14:39:24 +0100

    .

--- src/repositories/activities-repository/index.ts
+++ src/repositories/activities-repository/index.ts
@@ -1,5 +1,5 @@
-import { prisma } from '@/config';
-import { Activity, ActivityBooKing } from '@prisma/client';
+import { prisma } from "@/config";
+import { Activity, ActivityBooKing } from "@prisma/client";
 
 async function findActivities() {
   return prisma.activity.findMany({
