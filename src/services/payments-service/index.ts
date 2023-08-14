a4345646253807867f441e08af69f9b31b8eefab
Tiago <tiagoalex0202@gmail.com>
2023-08-14 12:16:28 +0100

    payment backend

--- src/services/payments-service/index.ts
+++ src/services/payments-service/index.ts
@@ -2,6 +2,7 @@ import { notFoundError, unauthorizedError } from "@/errors";
 import paymentRepository, { PaymentParams } from "@/repositories/payment-repository";
 import ticketRepository from "@/repositories/ticket-repository";
 import enrollmentRepository from "@/repositories/enrollment-repository";
+import sendPaymentConfirmation from "@/utils/payment-confirmation";
 
 async function verifyTicketAndEnrollment(ticketId: number, userId: number) {
   const ticket = await ticketRepository.findTickeyById(ticketId);
@@ -43,16 +44,42 @@ async function paymentProcess(ticketId: number, userId: number, cardData: CardPa
 
   await ticketRepository.ticketProcessPayment(ticketId);
 
+  const emailData = await ticketRepository.getInfoByEmail(ticketId);
+
+  let ticketType;
+
+  if (emailData) {
+    if (emailData.TicketType.isRemote === true) {
+      ticketType = "Online";
+    } else if (emailData.TicketType.isRemote === false) {
+      ticketType = "Presencial";
+      if (emailData.TicketType.includesHotel === false) {
+        ticketType += " + Sem hotel";
+      } else if (emailData.TicketType.includesHotel === true) {
+        ticketType += " + Com hotel";
+      }
+    }
+
+    const mailInfo = {
+      userEmail: emailData.Enrollment.User.email,
+      userName: emailData.Enrollment.name,
+      ticketType,
+      price: emailData.TicketType.price
+    };
+
+    sendPaymentConfirmation(mailInfo);
+  }
+
   return payment;
 }
 
 export type CardPaymentParams = {
-  issuer: string,
-  number: number,
-  name: string,
-  expirationDate: Date,
-  cvv: number
-}
+  issuer: string;
+  number: number;
+  name: string;
+  expirationDate: Date;
+  cvv: number;
+};
 
 const paymentService = {
   getPaymentByTicketId,
